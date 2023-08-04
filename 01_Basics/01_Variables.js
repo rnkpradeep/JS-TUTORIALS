e71b997632f074be764fcdd67bf0b94fe9857ebd
PRADEEP KUMAR <128138823+rnkpradeep@users.noreply.github.com>
2023-08-04 09:29:39 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="example.com"
+var accountPassword="121212"
+accountCity="Shimla"
+accountState;
+// accountId=121121
+// console.log(accountState);
+// console.log(accountId);
+
+accountEmail="example123.com"
+accountPassword="121111212"
+accountCity="Solan"
+// accountState;
+// console.table([accountId,accountEmail,accountPassword,accountState])
