07adbb226a8d0b720fd9c2e02e82bc4e499c7170
PRADEEP KUMAR <128138823+rnkpradeep@users.noreply.github.com>
2023-08-04 09:31:40 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all JS code as newer version
+
+// documentation for alert reference (MDN 0or tc39.es)
+
+let name ="Pradeep" //string  single or double courtes"
+let age=29//number
+let isLoggedIn=true//Boolean
+// Primitive Datatypes
+// number=> 2 to the power 53
+// BigInt
+// string=>"" or ''
+// Boolean=> true or false
+// null=>standalone value
+// undefined=> declared but not defined yet
+// symbol =>uniqueness mostly used in react for uniquness 
+
+
+// Non Premitive Datatype
+// object 
+// array
+ 
+
+
+
+
+console.log(typeof "name");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
