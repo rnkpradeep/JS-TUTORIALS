8479d8444879ccc5a528db9c0a46e8d8317ae22d
PRADEEP KUMAR <128138823+rnkpradeep@users.noreply.github.com>
2023-08-04 10:21:06 +0000

    conversion of Datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+let score=44
+// console.log(typeof score); //number  (lowercase)
+// console.log(typeof(score));// number (lowercase)
+// 
+let score1="ab"
+console.log(typeof(score1));
+let valueInNumber=Number(score1) // string to number
+console.log(typeof valueInNumber); //number
+// console.log(valueInNumber);  //NaN
+
+// there is no strict check in Javascript but in typescript has strict check
+// "44"=> 44
+// "55ab"=>NaN
+//null=>0
+//undefined=>NaN
+//true=> 1 or false=>0
+
+
